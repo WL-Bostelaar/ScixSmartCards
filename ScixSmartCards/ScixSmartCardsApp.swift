1329009bfbd2c9fe1ec72fa6021d93c1dca82c49
WL-Bostelaar <166092672+WL-Bostelaar@users.noreply.github.com>
2024-07-20 10:15:17 +1000

    Initial Commit

---
+++ ScixSmartCards/ScixSmartCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScixSmartCardsApp.swift
+//  ScixSmartCards
+//
+//  Created by William Bostelaar on 20/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScixSmartCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
